d5a70f97af0acce8b787294b01f46a27bdf4e516
Laura Benz <laura.m.k.benz@gmail.com>
2021-01-17 15:45:14 +0100

    feat: create test endpoint

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
-# Create your views here.
+
+class HelloApiView(APIView):
+    """test APIView"""
+
+    def get(self, request, format=None):
+        """returns a list"""
+
+        an_apiview = [
+            'test1', 'test2', 'test3'
+        ]
+
+        return Response({'message': 'Hello! This is a test', 'an_apiview': an_apiview})
