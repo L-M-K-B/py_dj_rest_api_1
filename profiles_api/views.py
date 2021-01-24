d6b7945abd1a83cf564e951450acfde2be228581
Laura Benz <laura.m.k.benz@gmail.com>
2021-01-24 12:25:00 +0100

    feat: add viewset class

--- profiles_api/views.py
+++ profiles_api/views.py
@@ -1,6 +1,6 @@
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import status, viewsets
 
 from profiles_api import serializers
 
@@ -42,3 +42,15 @@ class HelloApiView(APIView):
     def delete(self, request, pk=None):
         """delete an object"""
         return Response({'method': 'DELETE'})
+
+
+class HelloViewSet(viewsets.ViewSet):
+    """test api viewset"""
+
+    def list(self, request):
+        """return a message saying hello"""
+
+        a_viewset = [
+            'test4', 'test5', 'test6'
+        ]
+        return Response({'message': 'Hello!', 'a_viewset': a_viewset})
